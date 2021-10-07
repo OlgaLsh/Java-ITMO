64379a534eb68d1b2574441b28c926e941d3a367
OlgaLsh <73122567+OlgaLsh@users.noreply.github.com>
2021-10-07 18:24:30 +0300

    homework #1

---
+++ src/main/java/com/ifmo/lesson1/ThreeDigitsSum.java
@@ -0,0 +1,23 @@
+package com.ifmo.lesson1;
+
+public class ThreeDigitsSum {
+    /*
+    В переменной n хранится натуральное трёхзначное число.
+    Создайте программу, вычисляющую и выводящую на экран сумму цифр числа n.
+     */
+    public static void main(String[] args) {
+        int n = 123;
+
+        int sum = sum(n);
+
+        System.out.println(sum);
+    }
+
+    public static int sum(int n) {
+        int a = n/100;
+        int b = (n%100)/10;
+        int c = (n%100)%10;
+
+        return a+b+c;
+    }
+}
---
+++ src/main/java/com/ifmo/lesson1/TwoDigitsSum.java
@@ -0,0 +1,23 @@
+package com.ifmo.lesson1;
+
+public class TwoDigitsSum {
+    /*
+    В переменной n хранится натуральное двузначное число.
+    Создайте программу, вычисляющую и выводящую на экран сумму цифр числа n.
+     */
+    public static void main(String[] args) {
+        int n = 23;
+
+        int sum = sum(n);
+
+        System.out.println(sum);
+    }
+
+    public static int sum(int n) {
+
+        int a = n/10;
+        int b = n%10;
+
+        return a+b;
+    }
+}
